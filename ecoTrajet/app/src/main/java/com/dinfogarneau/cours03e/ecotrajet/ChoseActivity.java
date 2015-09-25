acdb8690b0b3aeef8ed427b341aeadf220049dee
GuillaumeSimard <chadguy2@hotmail.com>
2015-09-25 17:37:46 -0400

    changement mineur

--- ecoTrajet/app/src/main/java/com/dinfogarneau/cours03e/ecotrajet/ChoseActivity.java
+++ ecoTrajet/app/src/main/java/com/dinfogarneau/cours03e/ecotrajet/ChoseActivity.java
@@ -16,28 +16,6 @@ public class ChoseActivity extends Activity {
         setContentView(R.layout.activity_chose);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_chose, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
     //methode onClick lorsque l'on clique sur le bouton inscripption
     public void onClickPasager(View v)
     {
